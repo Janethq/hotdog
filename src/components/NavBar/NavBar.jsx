0a9a251172907d07e32707f61f6a7472a1f1bbf8
Jane <tanhuiqing95@hotmail.com>
2024-06-02 23:58:58 +0800

    feat: add tailwind for navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,14 +8,28 @@ export default function NavBar({ setUser }) {
   };
 
   return (
-    <nav>
-      <NavLink to="/profile">profile</NavLink>
+    <nav className="fixed top-0 left-0 w-64 h-screen flex flex-col bg-blue-500 text-white p-6 pt-12">
+      <NavLink
+        to="/profile"
+        className="py-2 px-4 hover:bg-gray-700 hover:text-white transition duration-300"
+      >
+        Profile
+      </NavLink>
       {/* orderhistory */}
-      &nbsp; | &nbsp;
-      <NavLink to="/appointments">Appointments</NavLink>
+      <div className="h-px bg-gray-700"></div>
+      <NavLink
+        to="/appointments"
+        className="py-2 px-4 hover:bg-gray-700 hover:text-white transition duration-300"
+      >
+        Appointments
+      </NavLink>
       {/* newORder */}
-      &nbsp;&nbsp;
-      <Link to="" onClick={handleLogOut}>
+      <div className="h-px bg-gray-700"></div>
+      <Link
+        to=""
+        onClick={handleLogOut}
+        className="py-2 px-4 hover:bg-gray-700 hover:text-white transition duration-300"
+      >
         Log Out
       </Link>
     </nav>
