2e197fca9417e4745542a08e3ac832e5bdf245b6
Jane <tanhuiqing95@hotmail.com>
2024-06-05 00:23:42 +0800

    feat: Profile

--- src/components/ProfileSection/ProfileSection.jsx
+++ src/components/ProfileSection/ProfileSection.jsx
@@ -1,20 +1,33 @@
-
-const Profile = () => {
-  // const handleEdit = () => {
-  // //   setIsEditing(true);
-  // };
-
-  // const handleSave = () => {
-  //   // onEdit({ name, dogName, breed, weight, username, password });
-  //   // setIsEditing(false);
-  // };
-
+const Profile = ({ username, password, name, dogName, breed, weight }) => {
   return (
     <>
-      <p>Deets</p>
+      <h2>Profile Details</h2>
+      <div>
+        <label>Name: </label>
+        <span>{name}</span>
+      </div>
+      <div>
+        <label>Dog Name: </label>
+        <span>{dogName}</span>
+      </div>
+      <div>
+        <label>Breed: </label>
+        <span>{breed}</span>
+      </div>
+      <div>
+        <label>Weight: </label>
+        <span>{weight} kg</span>
+      </div>
+      <div>
+        <label>Username: </label>
+        <span>{username}</span>
+      </div>
+      <div>
+        <label>Password: </label>
+        <span>{password}</span>
+      </div>
     </>
-   
   );
-}
-  
+};
+
 export default Profile;
