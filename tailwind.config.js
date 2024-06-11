cbb5f88a3926114d900839b439ec7ab9bb681cd0
Jane <tanhuiqing95@hotmail.com>
2024-06-12 03:24:52 +0800

    feat: font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Urbanist", "sans-serif"],
+    },
+    plugins: [],
   },
-  plugins: [],
 };
