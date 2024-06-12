24a7c74fbd5712bebc9709c55a7b70eebe47e17c
Jane <tanhuiqing95@hotmail.com>
2024-06-12 13:25:29 +0800

    feat: service name on map

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -5,8 +5,10 @@ import "./Map.css";
 
 export default function Map() {
   const [serviceAddresses, setServiceAddresses] = useState([]);
+    const [serviceNames, setServiceNames] = useState([]);
+
   const [mapCenter, setMapCenter] = useState([1.3521, 103.8198]);
-  const [zoom, setZoom] = useState(12);
+  const [zoom, setZoom] = useState(11);
 
   useEffect(() => {
     const fetchServices = async () => {
@@ -14,8 +16,9 @@ export default function Map() {
         const response = await fetch("/api/users/services");
         if (response.ok) {
           const data = await response.json();
-          console.log(data.map((service) => service.address));
+          console.log(data);
           setServiceAddresses(data.map((service) => service.address));
+          setServiceNames(data.map((service) => service.serviceName));
         } else {
           console.error("Failed to fetch services");
         }
@@ -27,7 +30,10 @@ export default function Map() {
   }, []);
 
   return (
-    <div style={{ height: "100vh", width: "100vw" }}>
+    <div
+      className="max-w-md mx-auto bg-transparent p-6 rounded-lg border border-transparent ml-0"
+      style={{ height: "100vh", width: "100vw" }}
+    >
       <MapContainer center={mapCenter} zoom={zoom}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
@@ -35,37 +41,38 @@ export default function Map() {
         />
         <Marker position={[1.3505233, 103.9243734]}>
           <Popup>
+            <span>{serviceNames[0]}</span> <br />
             <span>{serviceAddresses[0]}</span>
           </Popup>
         </Marker>
         <Marker position={[1.2933872, 103.8545593]}>
           <Popup>
+            <span>{serviceNames[1]}</span> <br />
             <span>{serviceAddresses[1]}</span>
           </Popup>
         </Marker>
         <Marker position={[1.3131482, 103.8970243]}>
           <Popup>
+            <span>{serviceNames[2]}</span> <br />
             <span>{serviceAddresses[2]}</span>
           </Popup>
         </Marker>
         <Marker position={[1.3528162, 103.9370613]}>
           <Popup>
+            <span>{serviceNames[3]}</span> <br />
             <span>{serviceAddresses[3]}</span>
           </Popup>
         </Marker>
         <Marker position={[1.3180642, 103.8959215]}>
           <Popup>
+            <span>{serviceNames[4]}</span> <br />
             <span>{serviceAddresses[4]}</span>
           </Popup>
         </Marker>
         <Marker position={[1.3441277, 103.7321283]}>
           <Popup>
+            <span>{serviceNames[5]}</span> <br />
             <span>{serviceAddresses[5]}</span>
-          </Popup>
-        </Marker>
-        <Marker position={[1.3521, 103.8198]}>
-          <Popup>
-            <span>{serviceAddresses[6]}</span>
           </Popup>
         </Marker>
       </MapContainer>
