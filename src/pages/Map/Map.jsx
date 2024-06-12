9c9dd1a5d77cb4cfdbc2fca39fa0ca6b00f31122
Jane <tanhuiqing95@hotmail.com>
2024-06-12 14:59:31 +0800

    fix: deploy map marker

--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -2,12 +2,19 @@ import { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "./Map.css";
+import { Icon } from "leaflet";
 
 export default function Map() {
   const [serviceAddresses, setServiceAddresses] = useState([]);
   const [serviceNames, setServiceNames] = useState([]);
   const [mapCenter, setMapCenter] = useState([1.3521, 103.8198]);
   const [zoom, setZoom] = useState(11);
+
+  const defaultIcon = new Icon({
+    iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+  });
 
   useEffect(() => {
     const fetchServices = async () => {
@@ -38,37 +45,37 @@ export default function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[1.3505233, 103.9243734]}>
+        <Marker icon={defaultIcon} position={[1.3505233, 103.9243734]}>
           <Popup>
             <span>{serviceNames[0]}</span> <br />
             <span>{serviceAddresses[0]}</span>
           </Popup>
         </Marker>
-        <Marker position={[1.2933872, 103.8545593]}>
+        <Marker icon={defaultIcon} position={[1.2933872, 103.8545593]}>
           <Popup>
             <span>{serviceNames[1]}</span> <br />
             <span>{serviceAddresses[1]}</span>
           </Popup>
         </Marker>
-        <Marker position={[1.3131482, 103.8970243]}>
+        <Marker icon={defaultIcon} position={[1.3131482, 103.8970243]}>
           <Popup>
             <span>{serviceNames[2]}</span> <br />
             <span>{serviceAddresses[2]}</span>
           </Popup>
         </Marker>
-        <Marker position={[1.3528162, 103.9370613]}>
+        <Marker icon={defaultIcon} position={[1.3528162, 103.9370613]}>
           <Popup>
             <span>{serviceNames[3]}</span> <br />
             <span>{serviceAddresses[3]}</span>
           </Popup>
         </Marker>
-        <Marker position={[1.3180642, 103.8959215]}>
+        <Marker icon={defaultIcon} position={[1.3180642, 103.8959215]}>
           <Popup>
             <span>{serviceNames[4]}</span> <br />
             <span>{serviceAddresses[4]}</span>
           </Popup>
         </Marker>
-        <Marker position={[1.3441277, 103.7321283]}>
+        <Marker icon={defaultIcon} position={[1.3441277, 103.7321283]}>
           <Popup>
             <span>{serviceNames[5]}</span> <br />
             <span>{serviceAddresses[5]}</span>
